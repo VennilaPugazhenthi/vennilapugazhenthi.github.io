661489dfcf395e459840007677dc1cd846210c64
Vennila Pugazhenthi <vennilapugazhenthi@gmail.com>
2020-05-26 09:42:21 -0400

    Up animation

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ selectElement('.close').addEventListener('click', () =>{
     selectElement('.nav-list').classList.remove('active');
 });
 
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
